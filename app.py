e2d2c95732a93a95e66d7a7b25d756f6b2e55838
Charlie Willmore <ccwillmore@gmail.com>
2021-06-17 15:02:26 -0500

    start module flask

--- app.py
+++ app.py
@@ -1,8 +1,32 @@
-from flask import Flask
+import numpy as np
+import pandas as pd
+import datetime as dt
+
+import sqlalchemy
+from sqlalchemy.orm import Session
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy import create_engine, func, inspect
+
+from flask import Flask, jsonify
+
+#Set up database using SQLAlchemy
+engine = create_engine("sqlite:///hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect = True)
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+session = Session(engine)
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello world'
-
+def Welcome():
+    return (
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
